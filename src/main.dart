af32405a4729f625ea1df174ea97ea04191522e2
portal2312 <portal2312@gmail.com>
2023-04-23 01:32:14 +0900

    1.3. Nullable Variables

--- src/main.dart
+++ src/main.dart
@@ -1,14 +1,5 @@
 void main() {
-  var one = 0;
-  one = 1;
-  var two = 'string';
-  // two = 2;  // Type Error.
-  String three = 'three';
-  var four;
-  dynamic five;
-  five = 'five';
-  five = 5;
-  if (five is String) {
-    print(five.length);
-  }
+  String? one = 'one';
+  one = null;
+  print(one?.isNotEmpty);
 }

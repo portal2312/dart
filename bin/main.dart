c2a75e6afd0523e4d130d33297fb1d0f35541039
portal2312 <portal2312@gmail.com>
2023-04-23 04:11:54 +0900

    1.7. Recap

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,27 @@
 void main() {
-  const one = 'one'; // Is compile-time constant.
-  // two = '2';  // Error: does not change.
-  // var api = unknownAPI();  // ok.
-  // late api = fetchAPI();  // ok.
-  // const api = fetchAPI();  // Error: Is not compile-time constant.
+  var one = 'one';
+  print(one);
+  String two = 'two';
+  print(two);
+  dynamic three;
+  three = 'three';
+  three = 3;
+  three = true;
+  print(three);
+  String? four = 'four';
+  four = null;
+  print(four?.isEmpty);
+  final five = 'five';
+  // five = 5;  // Error: does not change.
+  print(five);
+  late final six;
+  // print(six); // Error: definitely unassigned.
+  six = 'six';
+  print(six);
+  var seven = const [];
+  seven = ['seven'];
+  print(seven);
+  const eight = ['eight']; // Is compile-time constant.
+  // eight = [8]; // Error: does not change.
+  print(eight);
 }

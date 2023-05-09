550ebfb43d83bb6a81154810de7cde9a8483442c
portal2312 <portal2312@gmail.com>
2023-05-09 23:24:42 +0900

    2.2. String Interpolation

--- bin/main.dart
+++ bin/main.dart
@@ -1,24 +1,9 @@
 void main() {
-  var numbers = [1, 2, 3, 4];
-  print(numbers);
-  List<int> numbers2 = [1, 2, 3, 4];
-  numbers2.add(5);
-  print(numbers2);
-  var numbers3 = [
-    1,
-    2,
-    3,
-    4,
-    5,
-  ];
-  print(numbers3);
-  var giveMeFive = true;
-  var numbers4 = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5, // collection if.
-  ];
-  print(numbers4);
+  var name = 'MK';
+  var age = 35;
+  var greeting = '''
+  Hello everyone, my name is
+  $name and I'm ${age + 1}.
+  ''';
+  print(greeting);
 }

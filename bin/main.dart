b39a7fc443f4eef548e8c4d773803f1462661778
portal2312 <portal2312@gmail.com>
2023-06-23 15:15:03 +0900

    4.2. Named Constructor Parameters

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,15 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
     print('Hi, my name is $name.');
@@ -10,8 +17,18 @@ class Player {
 }
 
 void main() {
-  var player1 = Player('player1', 1500);
+  var player1 = Player(
+    name: 'player1',
+    xp: 1500,
+    team: 'red',
+    age: 12,
+  );
   player1.sayHello();
-  var player2 = Player('player2', 2500);
+  var player2 = Player(
+    name: 'player2',
+    xp: 2500,
+    team: 'blue',
+    age: 12,
+  );
   player2.sayHello();
 }

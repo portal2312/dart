6bbf18c6a1bb43e78905f4f952fdb0cf9753f9db
portal2312 <portal2312@gmail.com>
2023-06-23 15:51:02 +0900

    4.4. Recap

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,12 @@
 class Player {
   final String name;
-  int age, xp;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.age,
-    required this.xp,
-    required this.team,
-  });
-
-  Player.createBluePlayer({required String name, required int age})
-      : this.name = name,
-        this.age = age,
-        this.team = 'blue',
-        this.xp = 0;
-
-  Player.createRedPlayer(String name, int age)
-      : this.name = name,
-        this.age = age,
-        this.team = 'red',
-        this.xp = 0;
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 
   void sayHello() {
     print('Hi, my name is $name.');
@@ -28,8 +14,26 @@ class Player {
 }
 
 void main() {
-  var bluePlayer = Player.createBluePlayer(name: 'blue', age: 12);
-  bluePlayer.sayHello();
-  var redPlayer = Player.createRedPlayer('red', 24);
-  redPlayer.sayHello();
+  var apiData = [
+    {
+      "name": "A",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "B",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "C",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
